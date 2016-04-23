44c171a2512849a0f9acba2bdb0a5b18752a2209
Chris Dermody <chrisdermody1@gmail.com>
2016-04-23 14:57:14 +0100

    trying to concatenate the request onto the domain search API with Unirest, logging to heroku

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ app.get('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Domain checker app for Slack bot has started');
 });
 
 app.use(bodyParser());
 
 app.post('/', function (req, res) {
-	console.log('req is',req.body.text);
+	// console.log('req is',req.body.text);
 	//Use unirest to make the get request to our api
-	//Note: we'll concatenate the req to the url (that's our domain name)
-	// unirest.get("https://domainsearch.p.mashape.com/index.php?name="+req)
-	// .header("X-Mashape-Key", "l23VwmZ13TmshvRxySx2Bha4vG7vp1kC8mmjsnIgNGHmgpWcYn")
-	// .header("Accept", "application/json")
-	// .end(function (result) {
-	//   console.log(result.status, result.headers, result.body);
+	//Note: we'll concatenate the query to the url (that's our domain name)
+	unirest.get("https://domainsearch.p.mashape.com/index.php?name="+req.body.text)
+	.header("X-Mashape-Key", "l23VwmZ13TmshvRxySx2Bha4vG7vp1kC8mmjsnIgNGHmgpWcYn")
+	.header("Accept", "application/json")
+	.end(function (result) {
+	  console.log(result.status, result.headers, result.body);
 
-	// });
-  res.send('Received POST request');
+	});
+  res.send('Received POST request, check your logs');
 });
