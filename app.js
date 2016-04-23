e5c4027002eca91c34ecf5e4ba3e9273bcd49732
Chris Dermody <chrisdermody1@gmail.com>
2016-04-23 12:09:04 +0100

    added code to listen for port number in heroku or default to 3000 when running locally

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+var port_number = server.listen(process.env.PORT || 3000);
+
+app.listen(port_number, function () {
   console.log('Example app listening on port 3000!');
 });
