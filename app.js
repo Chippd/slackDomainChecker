3a23dac91940e008106051b3b19d66981fc19a3f
Chris Dermody <chrisdermody1@gmail.com>
2016-04-23 12:11:29 +0100

    update to port listening code

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
-
-app.listen(port_number, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
