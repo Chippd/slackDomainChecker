19c13513330acc3507ca4bbc89e94ccc9923823b
Chris Dermody <chrisdermody1@gmail.com>
2016-04-23 14:17:30 +0100

    testing what req is from slack

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var unirest = require('unirest');
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('This is a domain checker application built with node.js, Express.js and is running on Heroku. Send a POST request with the domain in question to receive information');
 });
 
 app.listen(process.env.PORT || 3000, function () {
@@ -12,5 +13,15 @@ app.listen(process.env.PORT || 3000, function () {
 
 
 app.post('/', function (req, res) {
+	console.log('req is':req)
+	//Use unirest to make the get request to our api
+	//Note: we'll concatenate the req to the url (that's our domain name)
+	// unirest.get("https://domainsearch.p.mashape.com/index.php?name="+req)
+	// .header("X-Mashape-Key", "l23VwmZ13TmshvRxySx2Bha4vG7vp1kC8mmjsnIgNGHmgpWcYn")
+	// .header("Accept", "application/json")
+	// .end(function (result) {
+	//   console.log(result.status, result.headers, result.body);
+
+	// });
   res.send('POST request to homepage');
 });
